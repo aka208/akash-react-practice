c4a51369e860532bc3646f2fd81fa019b7c5aa68
Akash Singh <2081993akash@gmail.com>
2024-03-28 11:02:53 +0530

    Exploring the world

--- namaste-react/src/components/Body.js
+++ namaste-react/src/components/Body.js
@@ -5,10 +5,11 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   //Local State Variable - Super powerful variable
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   // called after our component renders or render cycle completes
   useEffect(() => {
-    console.log("USE EFFECT CALLED");
     fetchData();
   }, []);
 
@@ -21,18 +22,47 @@ const Body = () => {
     setListOfRestaurants(
       json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
+    setFilteredRestaurants(
+      json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
   // Conditional Rendering
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer />;
+  // }
+
+  // setListOfRestaurants(listOfRestaurants);
 
   return listOfRestaurants.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              console.log(searchText);
+              const filteredList = listOfRestaurants.filter((restaurant) =>
+                restaurant.info.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(filteredList);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -40,7 +70,7 @@ const Body = () => {
             const filteredList = listOfRestaurants.filter(
               (res) => res.info.avgRating > 4
             );
-            setListOfRestaurants(filteredList);
+            setFilteredRestaurants(filteredList);
             console.log(listOfRestaurants);
           }}
         >
@@ -48,7 +78,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurants.map((restaurant) => (
           // if dont have key then use index as key
           // not using keys is not accptable <<<<<<< using index keys <<<<<<< using unique id as keys
           <ReastaurantCard key={restaurant.info.id} resData={restaurant.info} />
