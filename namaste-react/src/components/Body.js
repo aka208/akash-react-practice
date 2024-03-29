308993b286f5346713562924b9fdfdb9c9e8df7e
Akash Singh <2081993akash@gmail.com>
2024-03-29 12:46:42 +0530

    CORS issue resolved

--- namaste-react/src/components/Body.js
+++ namaste-react/src/components/Body.js
@@ -15,7 +15,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5967587&lng=73.896851&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5967587&lng=73.896851&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     //Optional Chaining
