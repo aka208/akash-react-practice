e5d46ba52b9579e1d9b597778dd770d460e1f946
Akash Singh <2081993akash@gmail.com>
2024-04-01 07:50:26 +0530

    Optimizing our App: Custom Hook Example 1

--- namaste-react/src/components/RestaurantMenu.js
+++ namaste-react/src/components/RestaurantMenu.js
@@ -1,37 +1,21 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { RESTAURANT_URL } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
-  console.log(resId);
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-  const fetchMenu = async () => {
-    const data = await fetch(
-      RESTAURANT_URL +
-        resId +
-        "&catalog_qa=undefined&isMenuUx4=true&submitAction=ENTER"
-    );
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
+  console.log(resInfo);
   if (resInfo === null) return <Shimmer />;
+  console.log(resInfo);
 
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[2]?.card?.card?.info;
 
   const { itemCards } =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
 
-  // const { itemCards } = resInfo;
-  //   if (resInfo === null) {
-  //     return <Shimmer />;
-  //   }
   return (
     <div className="menu">
       <h1>{name}</h1>
