4dc235ef4cf6cfd23820b87b838277b7b415c309
Akash Singh <2081993akash@gmail.com>
2024-04-06 23:56:58 +0530

    CSS changes

--- namaste-react/src/components/ItemList.js
+++ namaste-react/src/components/ItemList.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({ items }) => {
   console.log("ITEMS ", items);
   return (
@@ -7,19 +9,22 @@ const ItemList = ({ items }) => {
         {items.map((item) => (
           <div
             key={item.card.info.id}
-            className="p-2 m-2 border-b-2 border-gray-200 text-left"
+            className="p-2 m-2 border-b-2 border-gray-200 text-left flex"
           >
-            <div className="py-2">
-              <span>{item.card.info.name}</span>
-              <span>
-                {" "}
-                - ₹{" "}
-                {item.card.info.price
-                  ? item.card.info.price / 100
-                  : item.card.info.defaultPrice}
-              </span>
+            <img src={CDN_URL + item.card.info.imageId} className="w-20" />
+            <div>
+              <div className="py-2">
+                <span>{item.card.info.name}</span>
+                <span>
+                  {" "}
+                  - ₹{" "}
+                  {item.card.info.price
+                    ? item.card.info.price / 100
+                    : item.card.info.defaultPrice}
+                </span>
+              </div>
+              <p className="text-xs">{item.card.info.description}</p>
             </div>
-            <p className="text-xs">{item.card.info.description}</p>
           </div>
         ))}
       </div>
