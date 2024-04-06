cf35dcf8182e0cdab329ecdeb1afdf230733f110
Akash Singh <2081993akash@gmail.com>
2024-04-06 23:46:06 +0530

    Issue Fix for categor Items Likst

--- namaste-react/src/components/ItemList.js
+++ namaste-react/src/components/ItemList.js
@@ -2,17 +2,27 @@ const ItemList = ({ items }) => {
   console.log("ITEMS ", items);
   return (
     <div>
-      {items[0].card.info.name}
-      {items.map((item) => {
-        <div key={item.card.info.id} className="p-2 m-2 border brder-black">
-          <div>
-            Text
-            <span>{item.card.info.name}</span>
-            <span>{item.card.info.price}</span>
+      {/* {items[0].card.info.name} */}
+      <div>
+        {items.map((item) => (
+          <div
+            key={item.card.info.id}
+            className="p-2 m-2 border-b-2 border-gray-200 text-left"
+          >
+            <div className="py-2">
+              <span>{item.card.info.name}</span>
+              <span>
+                {" "}
+                - ₹{" "}
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice}
+              </span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <p>{item.card.info.description}</p>
-        </div>;
-      })}
+        ))}
+      </div>
     </div>
   );
 };
