f4a97709aba65e81f793830de75ca370adfd4c27
Akash Singh <2081993akash@gmail.com>
2024-05-05 20:56:20 +0530

    Header Test Cases

---
+++ namaste-react/src/__tests__/Header.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("should load Header component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginBtn = screen.getByRole("button");
+  expect(loginBtn).toBeInTheDocument();
+});
