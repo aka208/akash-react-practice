c46f5e1700d44e728f75c8521375269b3bf76553
Akash Singh <2081993akash@gmail.com>
2024-05-05 20:11:22 +0530

    Describe intro in Jest Test Cases

--- namaste-react/src/__tests__/Contact.test.js
+++ namaste-react/src/__tests__/Contact.test.js
@@ -2,34 +2,36 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../components/Contact";
 import "@testing-library/jest-dom";
 
-test("should load contact us component", () => {
-  render(<Contact />);
+describe("Contact Us page test cases", () => {
+  test("should load contact us component", () => {
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
-  expect(heading).toBeInTheDocument();
-});
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+  });
 
-test("should load button inside contact component", () => {
-  render(<Contact />);
+  test("should load button inside contact component", () => {
+    render(<Contact />);
 
-  const button = screen.getByRole("button");
-  const submitButton = screen.getByText("Submit");
-  expect(button).toBeInTheDocument();
-  expect(submitButton).toBeInTheDocument();
-});
+    const button = screen.getByRole("button");
+    const submitButton = screen.getByText("Submit");
+    expect(button).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
 
-test("should load input name inside contact component", () => {
-  render(<Contact />);
+  test("should load input name inside contact component", () => {
+    render(<Contact />);
 
-  const inputName = screen.getByPlaceholderText("name");
+    const inputName = screen.getByPlaceholderText("name");
 
-  expect(inputName).toBeInTheDocument();
-});
+    expect(inputName).toBeInTheDocument();
+  });
 
-test("should load 2 input boxes inside contact component", () => {
-  render(<Contact />);
+  test("should load 2 input boxes inside contact component", () => {
+    render(<Contact />);
 
-  const numberInputBoxes = screen.getAllByRole("textbox");
+    const numberInputBoxes = screen.getAllByRole("textbox");
 
-  expect(numberInputBoxes.length).toBe(2);
+    expect(numberInputBoxes.length).toBe(2);
+  });
 });
