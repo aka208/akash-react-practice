53125b48a0872f28006fa8f41508244a5af52ed2
Akash Singh <2081993akash@gmail.com>
2024-05-05 19:59:44 +0530

    Test cases for contact us page

--- namaste-react/src/__tests__/Contact.test.js
+++ namaste-react/src/__tests__/Contact.test.js
@@ -8,3 +8,28 @@ test("should load contact us component", () => {
   const heading = screen.getByRole("heading");
   expect(heading).toBeInTheDocument();
 });
+
+test("should load button inside contact component", () => {
+  render(<Contact />);
+
+  const button = screen.getByRole("button");
+  const submitButton = screen.getByText("Submit");
+  expect(button).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+test("should load input name inside contact component", () => {
+  render(<Contact />);
+
+  const inputName = screen.getByPlaceholderText("name");
+
+  expect(inputName).toBeInTheDocument();
+});
+
+test("should load 2 input boxes inside contact component", () => {
+  render(<Contact />);
+
+  const numberInputBoxes = screen.getAllByRole("textbox");
+
+  expect(numberInputBoxes.length).toBe(2);
+});
