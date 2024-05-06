5a0061efcd248a38d289d81619303e273edf0ab0
Akash Singh <2081993akash@gmail.com>
2024-05-06 22:53:38 +0530

    HOC RestaurantCard Test Cases

--- namaste-react/src/__tests__/RestaurantCard.test.js
+++ namaste-react/src/__tests__/RestaurantCard.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import ReastaurantCard from "../components/ReastaurantCard";
+import ReastaurantCard, {
+  WithPromotedLabel,
+} from "../components/ReastaurantCard";
 import MOCK_DATA from "./mocks/RestaurantCardMock.json";
 import "@testing-library/jest-dom";
 
@@ -7,4 +9,12 @@ it("should render RestaurantCard component with props data", () => {
   render(<ReastaurantCard resData={MOCK_DATA} />);
   const restName = screen.getByText("Chinese Wok");
   expect(restName).toBeInTheDocument();
+});
+
+it("should render RestaurantCard Component with Promoted label", () => {
+  // Test HOC withPromotedLabel()
+  const RestaurantWithPromotionLabel = WithPromotedLabel(ReastaurantCard);
+  render(<RestaurantWithPromotionLabel resData={MOCK_DATA} />);
+  const promotedLabel = screen.getByText("Promoted");
+  expect(promotedLabel).toBeInTheDocument();
 });
